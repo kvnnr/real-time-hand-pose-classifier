71c2a7c03fd4b51747a998c42c8cdf8f735563d5
Keven C Niere <kvnnr.business@gmail.com>
2026-06-25 06:01:35 +0800

    chore: update the name of fodler normalized -> normalizer

---
+++ src/feature/normalizer.py
@@ -0,0 +1,15 @@
+
+
+class Normalized:
+    """
+    Convert landmarks relative to wrist
+
+    Input:
+        UnNormalized 21 Hand landmarks | X Y Z
+    Process:
+        Translate landmarks relative to wrist  
+        Normalize hand size for scale invariance
+    Output:
+        Normalized 21 Hand Landmarks
+    """
+    pass
